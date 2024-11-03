66e1005af7e2d6a0a36b5f719b2d1f61e3776308
Mayank Jha <70832880+mayank7jha1@users.noreply.github.com>
2024-11-03 10:55:35 +0530

    Update Linked_List.h

--- Lecture 34/Linked_List.h
+++ Lecture 34/Linked_List.h
@@ -64,6 +64,7 @@ int MidPoint1(node<int>*head) {
 
 //MidPoint As a Node.
 node<int>* MidPoint2(node<int>*head) {
+
 	int ans = Length(head);
 	ans = ans / 2;
 
@@ -73,6 +74,106 @@ node<int>* MidPoint2(node<int>*head) {
 	}
 
 	return head;
+}
+
+//Middle Point as value and In case of even linked list we are returing second middle point.
+int MidPoint3(node<int>*head) {
+
+	node<int>*fast = head;
+	node<int>*slow = head;
+
+	while (fast != NULL and fast->next != NULL) {
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+
+	//You have the middle Point.
+	return slow->data;
+}
+
+
+//Middle Point as value and In case of even linked list we are returing first middle point.
+int MidPoint4(node<int>*head) {
+
+	node<int>*fast = head->next;//Different from Midpoint3.
+	node<int>*slow = head;
+
+	while (fast != NULL and fast->next != NULL) {
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+
+	//You have the middle Point.
+	return slow->data;
+}
+
+//Iteratively and Recursively Search a Key in Linked List.
+
+bool SearchIteratively(node<int>*head, int key) {
+
+	while (head != NULL) {
+
+		if (head->data == key) {
+			return true;
+		}
+
+		head = head->next;
+	}
+
+	return false;
+}
+
+
+/*
+	1.  F(): I want to search a node value from the head(50) to the end of the Linked list.
+	2.  G(): I want to search a node value from a different head(30) to the end of the Linked List.
+
+	1. F(head(50)); //State
+	2. F(head(30)); //State
+
+	50->40->30->20->10->NULL;
+
+	head=50;//I am here talking about the address i.e. head is a pointer and I am storing the address of the node 50 in head but for simplicity I am writing it as head=50.
+
+	F(50): This will return true or false.
+
+	F(50)=I will check the current node i.e if 50 is equal to key or not and ask recursion to do the same.
+
+	F(50)=Some Work + F(40);
+
+*/
+
+bool SearchRecursively(node<int>*head, int key) {
+
+	//Base Conditon:
+	if (head == NULL) {
+		return false;
+	}
+
+	//Work
+	if (head->data == key) {
+		return true;
+	}
+
+	//Recursion:
+	return SearchRecursively(head->next, key);
+}
+
+void ReverseIteratively(node<int>*&head) {
+
+	node<int>*prev = NULL;
+	node<int>*current = head;
+	node<int>*n = NULL; //Random: I want to avoid Giving a Pointer a garbage value.
+
+	while (current != NULL) {
+		n = current->next;
+		current->next = prev;
+		prev = current;
+		current = n;
+	}
+
+	head = prev;
+	return;
 }
 
 
